499f35814c8d7bab0b1c458ca957a5ac6091721e
Julius Bier Kirkegaard <juliusbierk@gmail.com>
2017-01-29 02:16:12 +0000

    Load planned path from db semi working

--- mysite/pathfeets/urls.py
+++ mysite/pathfeets/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^api/event/(?P<token>[a-zA-Z0-9]+)/$', views.details, name='details'),
+    url(r'^event/(?P<token>[a-zA-Z0-9]+)/$', views.event, name='details'),
     url(r'^api/send_gps/$', views.receive_gps_coord, name='send_gps'),
     url(r'^api/make_planned_pos/$', views.make_planned_pos, name='make_planned_pos'),
     url(r'^api/create/$', views.create_event, name='create event'),
