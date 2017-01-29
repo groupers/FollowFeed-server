c336cd655eb63c3e227c5a0097ebe17403095ca5
joelguo <jguo48@gmail.com>
2017-01-29 00:48:05 +0000

    event create

--- mysite/pathfeets/urls.py
+++ mysite/pathfeets/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^event/(?P<token>[a-zA-Z0-9]+)/$', views.details, name='details')
+    url(r'^event/(?P<token>[a-zA-Z0-9]+)/$', views.details, name='details'),
+    url(r'^create/$', views.create_event, name='create event')
 ]
